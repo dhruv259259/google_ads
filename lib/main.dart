5a013285ce88b4c7af51730fc5731656ba208327
dhruv259259 <115203169+dhruv259259@users.noreply.github.com>
2023-01-04 11:09:59 +0530

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize();
   runApp(MaterialApp(
     home: g_mod(),
     debugShowCheckedModeBanner: false,
@@ -10,7 +13,6 @@ void main() {
 }
 
 class g_mod extends StatefulWidget {
-  const g_mod({Key? key}) : super(key: key);
 
   @override
   State<g_mod> createState() => _g_modState();
@@ -18,14 +20,53 @@ class g_mod extends StatefulWidget {
 
 class _g_modState extends State<g_mod> {
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    myBanner.load();
+  }
+  BannerAd myBanner = BannerAd(
+    adUnitId: 'ca-app-pub-3940256099942544/6300978111ghk',
+    size: AdSize.smartBanner,
+    request: AdRequest(),
+    listener:  BannerAdListener(
+      // Called when an ad is successfully received.
+      onAdLoaded: (Ad ad) => print('Ad loaded.'),
+      // Called when an ad request failed.
+      onAdFailedToLoad: (Ad ad, LoadAdError error) {
+        // Dispose the ad here to free resources.
+        ad.dispose();
+        print('Ad failed to load: $error');
+      },
+      // Called when an ad opens an overlay that covers the screen.
+      onAdOpened: (Ad ad) => print('Ad opened.'),
+      // Called when an ad removes an overlay that covers the screen.
+      onAdClosed: (Ad ad) => print('Ad closed.'),
+      // Called when an impression occurs on the ad.
+      onAdImpression: (Ad ad) => print('Ad impression.'),
+    ),
+
+  );
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    myBanner.dispose();
+  }
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Google ad mode"),
       ),
-      body: Column(children: [
-        
-      ]),
+
+      bottomNavigationBar:Container(
+        alignment: Alignment.center,
+        child:  AdWidget(ad: myBanner),
+        width: double.infinity,
+        height: 50
+      ),
     );
   }
 }
